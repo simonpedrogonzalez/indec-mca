6faf8092a392d5c79423201bdc5091eb4365e5c2
Simón Pedro González <sgonzalez@travelpaq.com.ar>
2022-05-23 11:35:01 -0300

    everything

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # indec-mca
-R script to preprocess and analyze indec USU "hogar" and "individuos" microdatabases
+R scripts to preprocess and analyze indec USU "hogar" and "individuos" microdatabases.
+
+## How to use:
+1. **Install R 4.1.3 or later**
+2. **Download a dataset** from https://www.indec.gob.ar/indec/web/Institucional-Indec-BasesDeDatos, it has to be an Encuesta Permanente de Hogares xls file.
+3. **Put the downloaded data into the "input" folder** of the "individuos" or "hogar" folder, depending of the data you downloaded (Encuesta Individuos o Encuesta Hogares).
+4. **Look for the path to the script**.
+5. **Run the script**: navigate through the directories until you are in the same directory as `script.R` file, then run `R`, and then write in R console `source("script.R")`. You can also use RStudio or other tools to open the script and run it.
+6. **Check for the "result" folder**. There you can find:
+a. folder **"mca"** with Multiple Correspondance Analysis results and graphs.
+b. folder **"cluster"** with Hierarchical Clustering results and graphs.
+7. To use an script again, **remove or cut the result folder, the Rplot[number].pdf files and replace the input file** in the "input" folder.
+8. The code is commented, you can change any part at your own discretion. It is advisable to **run it and check the graphs to see if some values** (such as number of clusters, or dimensions taken into account, etc) **need to be changed**. Then run again.
+9. Variable renaming was done taking into account 2017 databases. You can change it editing the files in the "index" folder.
+
+
+## Process explained
+1. The script reads the data
+2. It changes numerical columns to intervals
+3. It changes column and category names to make them more readable
+4. It performs MCA and calculates the best number of dimensions to keep.
+5. It makes some graphs and writes MCA results.
+6. It performs HCPC and lets you decide the cutting hight in a dendogram.
+7. It makes some graphs and writes HCPC results.
---
+++ hogar/script.R
@@ -0,0 +1,320 @@
+print("Make sure you are useing R 4.1.3 or higher")
+print("Your version")
+print(version)
+options(warn=-1)
+print("Installing dependencies...")
+dependencies <- c("readxl", "readr", "data.table", "plyr",
+                  "dplyr", "ggplot2", "factoextra", "FactoMineR", 
+                  "corrplot", "cluster", "eeptools", "gridExtra",
+                  "tidyr", "ggrepel", "stringr", "writexl")
+
+new.packages <- dependencies[!(dependencies %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages, repos = "http://cran.us.r-project.org")
+
+dependencies2 <- c("readxl")
+new.packages2 <- dependencies2[!(dependencies2 %in% installed.packages()[,"Package"])]
+if(length(new.packages2)) withr::with_makevars(c(PKG_LIBS = "-liconv"), install.packages("readxl", repos = "http://cran.us.r-project.org"), assignment = "+=")
+
+
+dependencies3 <- c("ggplot2")
+new.packages3 <- dependencies3[!(dependencies3 %in% installed.packages()[,"Package"])]
+if(length(new.packages3)) install.packages(c('mgcv', 'ggplot2'), repos = "http://cran.us.r-project.org")
+
+# Loading
+library("readxl")
+library(readr)
+library(data.table)
+library(plyr)
+library(dplyr)
+library(ggplot2)
+library("factoextra")
+library(FactoMineR)
+library("corrplot")
+library(cluster)
+library(eeptools)
+library(gridExtra)
+library(tidyr)
+library(ggrepel)
+
+# READING DATA
+
+print("Reading data ...")
+
+filenames <- list.files("input", pattern="*.xls|*.xlsx", full.names=TRUE)
+
+if (length(filenames) == 0) {
+  stop("No input files found.")
+}
+df <- read_excel(filenames[1])
+
+# CREATE DIRECTORIES FOR THE RESULTS
+print("Creating result directories ...")
+dir.create('result')
+dir.create('result/cluster')
+dir.create('result/mca')
+
+# SELECT REGION. IF WANT TO SELECT ALL REGIONS, COMMENT THE FOLLOWING 2 LINES
+REGION_NUMBER <- 42 # example: San Juan is region 42
+df <- df[df$REGION == REGION_NUMBER, ]
+
+
+print("preprocessing ...")
+
+# SELECTING COLUMNS. YOU CAN CHANGE THE SELECTED
+# COLUMNS. REMEMBER TO KEEP THE ORDER SO THE
+# TRANSLATION IS DONE CORRECTLY. IF YOU DO NOT USE
+# A COLUMN, MAKE SURE TO CHECK IF ANY PART OF THE CODE
+# USES IT.
+
+c.hogar <- c("IV1","IV6","IV8","IV10","IV11","IV12_1","IV12_2","IV12_3","II4_1","II4_2","II4_3","II7","II8",'IV7')
+strat <- c("V1","V2","V3","V5","V6","V7","V11","V12","V13","V14","V15","V16","V17", 'V10', 'V22', 'V18')
+id <- c('CODUSU','NRO_HOGAR')
+num <- c("IX_TOT","IPCF")
+
+vars <-c(id,c.hogar,strat,num)
+
+vars.no.id <- setdiff(vars,c('CODUSU','NRO_HOGAR'))
+df<-df[,vars]
+
+# TRANSFORM NUMERICAL VARIABLES TO INTERVALS
+# Cantidad de miembros del hogar
+df$IX_TOT <- cut(df$IX_TOT, breaks=c(1, 2, 3, 5, 7, 10, 15, Inf), right = FALSE)
+# Ingresos per cápita familiar
+df$IPCF <- cut(df$IPCF,
+                breaks=c(0, 5000, 10000, 30000, 60000, 100000, Inf),
+                right = FALSE,
+               labels= c('[$0,$5.000)','[$5.000,$10.000)','[$10.000,$30.000)',
+                        '[$30.000,$60.000)','[$60.000,$100.000)','[$100.000,Inf)'))
+
+
+# TRANSFORM ALL VARS TO FACTORS
+df[] <- lapply(df[], factor)
+
+
+# TRANSLATE COLUMN NAMES AND VALUEST TO SOMETHING MORE READABLE
+# USING THE TRANSLATION FILES. YOU CAN CHANGE THE TRANSLATION
+# IN THOSE FILES IF YOU NEED TO, BUT MAKE SURE TO CHECK IF
+# ANY PART OF THE CODE USES THE TRANSLATED NAMES.
+
+dfiv <- read_excel("index/INDICE_PREGUNTAS_USU_HOGAR.xlsm")
+dfir <- read_excel("index/INDICE_RESPUESTAS_USU_HOGAR.xlsm")
+dfa <- df
+# Change category names
+for (colname in colnames(dfa)){
+    values <- unlist(c(dfir[dfir$COD == colname,"VALUE" ]),use.names = FALSE)
+    responses <- unlist(c(dfir[dfir$COD == colname,"RESPONSE" ]),use.names = FALSE)
+    dfa[colname] <- lapply(dfa[colname], function(x) mapvalues(unlist(x), from = values, to = responses))
+}
+# Change var names
+oldnames <- colnames(dfa)
+newnames <- unlist(c(dfiv[dfiv$COD %in% oldnames, "NAME"]), use.names = FALSE)
+setnames(dfa, oldnames, newnames)
+#Apply changes
+df <- dfa
+
+# Change var lists
+vars <- unlist(c(dfiv[dfiv$COD %in% vars, "NAME"]), use.names = FALSE)
+id <- unlist(c(dfiv[dfiv$COD %in% id, "NAME"]), use.names = FALSE)
+vars.no.id <- unlist(c(dfiv[dfiv$COD %in% vars.no.id, "NAME"]), use.names = FALSE)
+c.hogar <- unlist(c(dfiv[dfiv$COD %in% c.hogar, "NAME"]), use.names = FALSE)
+num <- unlist(c(dfiv[dfiv$COD %in% num, "NAME"]), use.names = FALSE)
+strat <- unlist(c(dfiv[dfiv$COD %in% strat, "NAME"]), use.names = FALSE)
+
+
+
+# CHECK NANS AND FACTORS WITH ONLY 1 LEVEL/CATEGORY
+print("Check columns with nans:")
+print(colnames(df)[colSums(is.na(df)) > 0])
+print("Check columns with only 1 category/factor:")
+print(ifelse(n <- sapply(df, function(x) length(levels(x))) == 1,'WARNING: 1 LEVEL','OK'))
+print("Check categories have more levels than used:")
+ifelse(n <- sapply(df, function(x) length(levels(x)) != length(unique(x))),'WARNING','OK')
+
+
+
+# REMOVE COLUMNS WITH ONLY 1 LEVEL/CATEGORY
+print("Removing columns with less than 2 levels/categories")
+cols_to_remove <- vars.no.id[sapply(df[,vars.no.id], function(x) length(levels(x)) < 2)]
+df <- df[, !names(df) %in% cols_to_remove]
+
+
+
+# CREATE A TABLE TO SHOW PREPROCESSING RESULT
+sample <- df[,names(df) %in% vars.no.id]
+sample <- sample[0:30,]
+library(gridExtra)
+png("result/sample.png", height = 25*nrow(sample), width = 300*ncol(sample), )
+grid.table(sample)
+dev.off()
+
+
+
+# CREATE SUMMARY BARPLOT
+library(tidyr)
+
+ggplot(gather(df[,names(df) %in% vars.no.id]), aes(value))+
+#scale_x_discrete(labels = abbreviate)+
+
+theme(axis.text.x = element_text(angle = 45,hjust=0.9,vjust=0.9,size=12),
+     axis.title=element_text(size=14,face="bold"))+
+geom_bar()+
+#coord_flip()+
+facet_wrap(~key, scales = 'free_x')
+ggsave("result/summary.png",width=10,height=10)
+
+
+
+# PERFORM MCA ANALYSIS
+# TAKE A LOOK AT
+# http://www.sthda.com/english/articles/31-principal-component-methods-in-r-practical-guide/114-mca-multiple-correspondence-analysis-in-r-essentials/
+# TO SEE A SIMPLE TUTORIAL
+
+print("Performing MCA analysis...")
+
+# FIRST PERFORM MCA TO CHECK ACUMULATIVE EXPLAINED VARIANCE
+var.mca <- c(c.hogar, strat, num)
+df.for.mca<-df[,names(df) %in% var.mca]
+mca <- MCA(df.for.mca,graph = FALSE)
+
+# SET NUMBER OF DIMENSION TO KEEP AS THE NUMBER
+# OF DIIMENSIONS THAT ACCUMULATIVELY EXPLAIN MORE THAN 60%
+# OF VARIANCE
+ncp <- sum(mca$eig[,3] < 60, na.rm=TRUE) + 1
+print("Keeping")
+print(ncp)
+print("dimensions...")
+mca <- MCA(df.for.mca,graph = FALSE, ncp=ncp)
+
+
+print("Creating plots...")
+
+# PLOT INERTIA DISTRIBUTION IN DIMENSIONS (SCREEPLOT) AND SAVE IN FILE
+fviz_screeplot(mca, addlabels = TRUE, ylim = c(0, 20))+theme(
+    axis.text=element_text(size=12),
+    axis.title=element_text(size=14,face="bold"),
+    plot.background = element_rect(fill = "white")
+)
+ggsave("result/mca/screeplot.png",width=5,height=5)
+
+
+# PLOT PROJECTED DATA IN FIRST 2 DIMENSIONS AND SAVE IN FILE
+# USING IPCF AS THE LABELING VARIABLE
+fviz_mca_biplot(mca,
+                repel = TRUE,
+                label=c("var"),
+                axes = c(2,1),
+                alpha.ind=0.05,
+                habillage = 'IPCF',
+                #palette = "Dark",
+                #addEllipses = TRUE,# ellipse.type = "confidence",
+               #ellipse.level=0.5,
+                xlim = c(-1.5, 1.5),
+                ylim = c(-1.5, 1.5),
+               ggtheme = theme_minimal())+theme(
+                 axis.text=element_text(size=12),
+                 axis.title=element_text(size=14,face="bold"),
+                 plot.background = element_rect(fill = "white")
+                 )
+ggsave("result/mca/biplot_IPCF.png",width=10,height=15)
+
+
+# OUTPUT MCA ANALYSIS DATA TO A TXT FILE
+cat("mca", file = "result/mca/mca.txt")
+capture.output(summary(mca), file = "result/mca/mca.txt", append = TRUE)
+
+
+# HIERARCHICAL CLUSTERING
+# YOU CAN CHOOSE A DIFFERENT NUMBER OF CLUSTERS BASED ON THE DENDOGRAM
+# CHECK
+# http://www.sthda.com/english/articles/31-principal-component-methods-in-r-practical-guide/117-hcpc-hierarchical-clustering-on-principal-components-essentials/
+# TO SEE A SIMPLE TUTORIAL
+print("Performing hierarchical clustering...")
+num.of.clust <- 0 # 0 lets you select a higth in the dendogram
+hcpc <- HCPC(mca, nb.clust = num.of.clust, graph = TRUE)
+
+num.of.clust <- length(unique(hcpc$data.clust$clust))
+
+
+# PLOT A DENDOGRAM
+print("Creating dendogram, this may take some time, please wait...")
+fviz_dend(hcpc, 
+          cex = 0.7, # Label size
+          show_labels = TRUE,
+          palette = "jco",
+          rect = FALSE,
+          #k=2,
+          #rect_fill = TRUE, # Add rectangle around groups
+          #rect_border = "aaas", # Rectangle color
+          labels_track_height = 0# Augment the room for labels
+          )+theme_classic()
+ggsave("result/cluster/dendogram.png",width=5,height=7)
+
+
+# OUTPUT HCPC ANALYSIS DATA TO A TXT FILE
+cat("clusters", file = "result/cluster/clusters.txt")
+capture.output(hcpc$desc.var, file = "result/cluster/clusters.txt", append = TRUE)
+
+
+# GRAPH A SUMMARY OF EACH CLUSTER
+library(tidyr)
+
+for (n in 1:num.of.clust){
+    
+    df.to.sum<-hcpc$data.clust[hcpc$data.clust$clust==n,]
+    df.to.sum<-df.to.sum[,!names(df.to.sum) %in% c('clust')]
+    
+    if(nrow(df.to.sum)<0){
+        print('Ignoring outliers')
+    }
+    else{
+        print(nrow(df.to.sum))
+        image.name=paste(paste("result/cluster/cluster",toString(round(nrow(df.to.sum)/nrow(df.for.mca)*100, digits = 2))),"pc.png")
+        ggplot(gather(df.to.sum), aes(value))+
+        #scale_x_discrete(labels = abbreviate)+
+        theme(axis.text.x = element_text(angle = 45,hjust=0.9,vjust=0.9,size=12),
+     axis.title=element_text(size=14,face="bold"))+
+        geom_bar()+
+        #coord_flip()+
+        facet_wrap(~key, scales = 'free_x')
+        ggsave(image.name,width=20,height=40)
+    } 
+}
+
+
+# PLOT CLUSTERED DATA IN FIRST 2 DIMENSIONS
+print("Creating clustering biplot...")
+
+var.mca <- c(c.hogar, num)
+df.for.mca <- df[,names(df) %in% var.mca]
+df.for.mca <- cbind(df.for.mca, Cluster = hcpc$data.clust$clust)
+
+mca <- MCA(df.for.mca, graph = FALSE, ncp=ncp,
+           # exclude cluster label as an active mca column, use only for graphs
+           quali.sup = c(grep("Cluster", colnames(df.for.mca)))
+          )
+fviz_mca_biplot(mca,
+                repel = TRUE,
+                label=c("var"),
+                axes = c(2,1),
+                alpha.ind=0.05,
+                habillage = 'Cluster',
+                palette = "Dark",
+                addEllipses = TRUE,# ellipse.type = "confidence",
+                ellipse.level=0.5,
+                xlim = c(-1, 2),
+                ylim = c(-0.5, 1.5),
+               ggtheme = theme_minimal())+theme(
+                 axis.text=element_text(size=12),
+                 axis.title=element_text(size=14,face="bold"),
+                 plot.background = element_rect(fill = "white")
+                )
+ggsave("result/cluster/biplot_clusters.png",width=16,height=10)
+
+
+# OUTPUT PREPROCESSED DATA TO A CSV FILE
+print("Writing preprocessed data to csv file...")
+library(writexl)
+write.csv(df, file = "result/preprocessed_data.csv",row.names=FALSE)
+
+print("Done!")
+print("Remember to remove the result folder and Rplot files before running the next time!")
